cc7b5b0c3da8af544a12760e85a8ffc81bf6fe74
GayanGamage96 <gayankmgamage@gmail.com>
2024-08-23 14:43:43 +0530

    implement new appointment form and functions

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,9 +1,17 @@
-"use server"
+"use server";
 import { ID, Query } from "node-appwrite";
-import { BUCKET_ID, DATABASE_ID, databases, ENDPOINT, PATIENT_COLLECTION_ID, PROJECT_ID, storage, users } from "../appwrite.config";
+import {
+  BUCKET_ID,
+  DATABASE_ID,
+  databases,
+  ENDPOINT,
+  PATIENT_COLLECTION_ID,
+  PROJECT_ID,
+  storage,
+  users,
+} from "../appwrite.config";
 import { parseStringify } from "../utils";
-import {InputFile} from 'node-appwrite/file'
-
+import { InputFile } from "node-appwrite/file";
 
 export const createUser = async (user: CreateUserParams) => {
   try {
@@ -36,29 +44,45 @@ export const getUser = async (userId: string) => {
   }
 };
 
-export const registerPatient = async ({identificationDocument,...patient}:RegisterUserParams) => {
-try {
-  let file;
-  if(identificationDocument){
-    const inputFile = InputFile.fromBuffer(
-      identificationDocument?.get('blobFile') as Blob,
-      identificationDocument?.get('fileName') as string
+export const getPatient = async (userId: string) => {
+  try {
+    const patients = await databases.listDocuments(
+      DATABASE_ID!,
+      PATIENT_COLLECTION_ID!,
+      [Query.equal("userId", userId)]
     );
-    file = await storage.createFile(BUCKET_ID!,ID.unique(),inputFile)
+    return parseStringify(patients.documents[0]);
+  } catch (error) {
+    console.log(error);
   }
-  const newPatient = await databases.createDocument(
-    DATABASE_ID!,
-    PATIENT_COLLECTION_ID!,
-    ID.unique(),
-    {
-      identificationDocumentId:file?.$id||null,
-      identificationDocumentUrl:`${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file?.$id}/view?project=${PROJECT_ID}`,
-      ...patient
+};
+
+export const registerPatient = async ({
+  identificationDocument,
+  ...patient
+}: RegisterUserParams) => {
+  try {
+    let file;
+    if (identificationDocument) {
+      const inputFile = InputFile.fromBuffer(
+        identificationDocument?.get("blobFile") as Blob,
+        identificationDocument?.get("fileName") as string
+      );
+      file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile);
     }
-  )
-  
-  return parseStringify(newPatient);
-} catch (error) {
-  console.log(error)
-}
+    const newPatient = await databases.createDocument(
+      DATABASE_ID!,
+      PATIENT_COLLECTION_ID!,
+      ID.unique(),
+      {
+        identificationDocumentId: file?.$id || null,
+        identificationDocumentUrl: `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file?.$id}/view?project=${PROJECT_ID}`,
+        ...patient,
+      }
+    );
+
+    return parseStringify(newPatient);
+  } catch (error) {
+    console.log(error);
+  }
 };
