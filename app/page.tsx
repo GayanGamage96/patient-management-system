6e242de8894aac6ea19002a80b90b375cfc40080
GayanGamage96 <gayangamagercg1996@gmail.com>
2025-03-02 22:28:21 +0530

    change banner

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home({ searchParams }: SearchParamProps) {
         </div>
       </section>
       <Image
-        src="/assets/images/onboarding-img.png"
+        src="/assets/images/banner.jpg"
         alt="patient"
         height={1000}
         width={1000}
